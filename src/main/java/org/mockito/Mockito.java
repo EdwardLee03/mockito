c19919055d5d8c95efd0999d27eb8f63e9036cb5
dannong <dannong@wacai.com>
2019-11-14 13:42:32 +0800

    [example] 8. Finding redundant invocations 查找多余的调用

--- src/main/java/org/mockito/Mockito.java
+++ src/main/java/org/mockito/Mockito.java
@@ -448,7 +448,8 @@ import org.mockito.verification.VerificationWithTimeout;
  *
  *
  *
- * <h3 id="8">8. <a class="meaningful_link" href="#finding_redundant_invocations" name="finding_redundant_invocations">Finding redundant invocations</a></h3>
+ * <h3 id="8">8. <a class="meaningful_link" href="#finding_redundant_invocations" name="finding_redundant_invocations">
+ *     Finding redundant invocations 查找多余的调用</a></h3>
  *
  * <pre class="code"><code class="java">
  * //using mocks
@@ -466,10 +467,16 @@ import org.mockito.verification.VerificationWithTimeout;
  * <code>verifyNoMoreInteractions()</code> is not recommended to use in every test method.
  * <code>verifyNoMoreInteractions()</code> is a handy assertion from the interaction testing toolkit. Use it only when it's relevant.
  * Abusing it leads to <strong>overspecified</strong>, <strong>less maintainable</strong> tests.
+ * 一个警告：一些做过很多经典的"期望-运行-验证"模式的用户，
+ * 往往会经常使用verifyNoMoreInteractions()，即使在每个测试方法中也是如此。
+ * 不建议在每个测试方法中都使用verifyNoMoreInteractions()。
+ * verifyNoMoreInteractions()是来自交互测试工具包的便捷断言。
+ * 仅在相关时使用，滥用它会导致规范过度且难以维护的测试。
  *
  * <p>
  * See also {@link Mockito#never()} - it is more explicit and
  * communicates the intent well.
+ * 另请参见{@link Mockito#never()}，更加明确且可以很好地传达意图。
  * <p>
  *
  *
